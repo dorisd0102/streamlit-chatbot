6a1938289c189cf9bf260c37d25890c2dd836128
dorisd0102 <74578288+dorisd0102@users.noreply.github.com>
2025-05-21 14:37:28 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,6 +31,7 @@ for message in st.session_state.messages:
 # Accept user input
 if prompt := st.chat_input("What is up?"):
     # Add user message to chat history
+    print(prompt)
     st.session_state.messages.append({"role": "user", "content": prompt})
     # Display user message in chat message container
     with st.chat_message("user"):
